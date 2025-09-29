836bb383ac19e299b967977f975a3f37437b8fd4
mtejareddy <manukondatejareddy@gmail.com>
2025-09-29 22:25:30 +0530

    colors

--- roboshop.sh
+++ roboshop.sh
@@ -8,7 +8,8 @@ DOMAIN_NAME="devopsdaws.store"
 
 for instance in ${INSTANCES[@]}
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-017f7590c2fde5628 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=test}]" --query "Instances[0].PrivateIpAddress" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-017f7590c2fde5628 --tag-specifications 
+    "ResourceType=instance,Tags=[{Key=Name, Value=test}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
