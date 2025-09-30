f7f900e3b3d02cc005414f761afe40cd16fec2fc
mtejareddy <manukondatejareddy@gmail.com>
2025-09-30 20:58:46 +0530

    colors

--- catalogue.sh
+++ catalogue.sh
@@ -45,7 +45,7 @@ VALIDATE $? "Installing nodejs:20"
 useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop & >>$LOG_FILE
 VALIDATE $? "Creating roboshop system user"
 
-mkdir /app 
+mkdir -p /app 
 VALIDATE $? "Creating app directory"
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip & >>$LOG_FILE
