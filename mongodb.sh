8ecc6b96d3b900c63dd09b44ccc76a4f09830e6e
mtejareddy <manukondatejareddy@gmail.com>
2025-09-30 12:17:53 +0530

    colors

--- mongodb.sh
+++ mongodb.sh
@@ -33,19 +33,19 @@ VALIDATE(){
 }
 
 cp mongo.repo /etc/yum.repos.d/mongodb.repo
-VALIDATE $? "Copying MONGODB repo"
+VALIDATE $? "Copying MongoDB repo"
 
 dnf install mongodb-org -y &>>$LOG_FILE
-VALIDATE $? "Installing Mongodb Server"
+VALIDATE $? "Installing mongodb Server"
 
 systemctl enable mongod &>>$LOG_FILE
-VALIDATE $? "Enabling MONGODB"
+VALIDATE $? "Enabling MongoDB"
 
 systemctl start mongod &>>$LOG_FILE
-VALIDATE $? "Starting MONGODB"
+VALIDATE $? "Starting MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-VALIDATE $? "Editing MONGODB conf file for remote connections"
+VALIDATE $? "Editing MongoDB conf file for remote connections"
 
 systemctl restart mongod &>>$LOG_FILE
-VALIDATE $? "Restarting MONGODB"
+VALIDATE $? "Restarting MongoDB"
